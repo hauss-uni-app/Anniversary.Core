4b023becf54a7c677d425e5368c9228393426cb8
Administrator <Administrator@HAUSS>
2020-06-21 20:43:02 +0800

    Add Poliy

---
+++ Anniversary/Infrastructure/ResilienceClientFactory.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Polly;
+using Resilience;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Anniversary.Infrastructure
+{
+    public class ResilienceClientFactory
+    {
+
+        private ILogger<ResilienceHttpClient> _logger;
+        private IHttpContextAccessor _httpContextAccessor;
+
+        //重试次数
+        private int _retryCount;
+
+        //熔断之前允许的异常次数
+        private int _exceptionCountAllowedBeforeBreaking;
+
+        public ResilienceClientFactory(ILogger<ResilienceHttpClient> logger,
+            IHttpContextAccessor httpContextAccessor,
+            int retryCount,
+            int exceptionCountAllowedBeforeBreaking)
+        {
+            _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+            _retryCount = retryCount;
+            _exceptionCountAllowedBeforeBreaking = exceptionCountAllowedBeforeBreaking;
+        }
+
+        public ResilienceHttpClient GetResilienceHttpClient() =>
+            new ResilienceHttpClient(origin => CreatePolicy(origin), _logger, _httpContextAccessor);
+
+        private IAsyncPolicy[] CreatePolicy(string origin) => new AsyncPolicy[]
+        {
+                Policy.Handle<HttpRequestException>()
+                .WaitAndRetryAsync(_retryCount,
+                    retryAttempt=>TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (exception,timeSpan,retryCount,context)=>
+                    {
+                        var msg= $"第 {retryCount} 次重试 "+
+                                $"of {context.PolicyKey} "+
+                                $"at {context.OperationKey}, "+
+                                $"due to：{exception}. ";
+                        _logger.LogWarning(msg);
+                        _logger.LogDebug(msg);
+                    }),
+                Policy.Handle<HttpRequestException>()
+                .CircuitBreakerAsync(
+                    _exceptionCountAllowedBeforeBreaking,
+                    TimeSpan.FromMinutes(1), //熔断持续时间
+                    (exception,duration)=>
+                    {
+                        _logger.LogTrace("熔断器打开");
+                    },()=>{
+                         _logger.LogTrace("熔断器关闭");
+                    })
+
+            };
+
+    }
+}
